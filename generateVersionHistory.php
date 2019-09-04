5ecbf2305e52cb610567902f83ffbea098e3772a
ReznoRMichael <mic3arena@gmail.com>
2019-09-04 16:49:09 +0200

    Replaced actions with cards, added links

--- generateVersionHistory.php
+++ generateVersionHistory.php
@@ -5,7 +5,52 @@ $strJsonFileContents = file_get_contents("GXLc34hk.json");
 $arrayJSON = json_decode($strJsonFileContents, true);
 // var_dump($arrayJSON); // print array
 
-function generateVersionHistory($arrayJSON) {
+function generateCardVersionHistory($arrayJSON) {
+    $releaseHistoryId = "5c8aded82d38c74039cf8009"; // the id of Version History on Trello
+    $searchResult = ""; $allEntries = ""; $cardID = "";
+    echo ""; // clear the contents each time the function is called
+
+    $elem = 0;
+    foreach($arrayJSON["cards"] as $i) {
+        if(array_key_exists( "idList", $i)) {
+            $searchResult = $i["idList"];
+
+            if($searchResult == $releaseHistoryId) {
+                $entryDate = substr($i["dateLastActivity"], 0, 10) . " — " . substr($i["dateLastActivity"], 11, 8); // "2019-08-28T17:56:51.603Z";
+                $entryURL = $i["url"];
+
+                if(array_key_exists("desc", $i)) {
+                    $entryProgram = $i["name"];
+                    if(ctype_space($i["desc"]) || strlen($i["desc"]) < 1) {
+                        $entryLog = "(No Patch Release description was given for this entry)";
+                    } else {
+                        $entryLog = str_replace("\n", "<br>", $i["desc"]);
+                    }
+                } else {
+                    $entryProgram = $i["name"];
+                    $entryLog = "(No Patch Release description was given for this entry)";
+                }
+
+                $entry = [
+                    "<div>",
+                        "<div style='border-bottom:1px solid lightgray;'></div>",
+                        // "<div class='entryNo'>" . $elem . "</div>",
+                        "<div class='date'>" . $entryDate . "</div>",
+                        "<p><a href='" . $entryURL . "' target='_blank' title='Click to open the link to the Trello card in a separate window'><strong>" . $entryProgram . "</strong></a></p>",
+                        "<p class='changelog'>" . $entryLog . "</div>",
+                    "</div>"
+                ];
+                $entry = implode("\n", $entry);
+            
+                $allEntries .= $entry;
+            }
+        }
+        $elem++;
+    }
+    echo $allEntries;
+}
+
+function generateActionVersionHistory($arrayJSON) {
     $releaseHistoryId = "5c8aded82d38c74039cf8009"; // the id of Version History on Trello
     $searchResult = ""; $allEntries = "";
     echo ""; // clear the contents each time the function is called
@@ -18,7 +63,6 @@ function generateVersionHistory($arrayJSON) {
             if($searchResult == $releaseHistoryId) {
                 // $entryDate = DateTime::createFromFormat('Y-m-dTH:i:s.u', str_replace("Z", "", $i["date"]))->format('Y-m-d');
                 // var_dump($entryDate);
-                "2019-08-28T17:56:51.603Z";
                 $entryDate = substr($i["date"], 0, 10) . " — " . substr($i["date"], 11, 8);
                 if(array_key_exists("card", $i["data"])) {
                     if(array_key_exists("desc", $i["data"]["card"])) {
@@ -46,16 +90,6 @@ function generateVersionHistory($arrayJSON) {
         }
         $elem++;
     }
-
-    /* ------------------------------------------------------------------------------------ */
-    // View all read entries in the #generate div
-    /* ------------------------------------------------------------------------------------ */
-
-    function viewAllEntries($allEntries) {
-        echo $allEntries;
-    }
-
-    viewAllEntries($allEntries);
 }
 
-generateVersionHistory($arrayJSON);
+generateCardVersionHistory($arrayJSON);
