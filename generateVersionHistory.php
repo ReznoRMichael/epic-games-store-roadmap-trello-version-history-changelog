b56215fd630df39906697487613b8def3fa08c1f
ReznoRMichael <mic3arena@gmail.com>
2019-09-05 15:07:39 +0200

    Formatted document

--- generateVersionHistory.php
+++ generateVersionHistory.php
@@ -14,10 +14,11 @@ echo ""; // clear the contents each time
  * @param mixed $arrayJSON
  * @return string
  */
-function getCreateCardDate($cardID, $arrayJSON) {
-    foreach($arrayJSON["actions"] as $i) {
-        if(array_key_exists("card", $i["data"])) {
-            if($i["data"]["card"]["id"] === $cardID && $i["type"] === "createCard") {
+function getCreateCardDate($cardID, $arrayJSON)
+{
+    foreach ($arrayJSON["actions"] as $i) {
+        if (array_key_exists("card", $i["data"])) {
+            if ($i["data"]["card"]["id"] === $cardID && $i["type"] === "createCard") {
                 return $i["date"];
             }
         }
@@ -30,24 +31,28 @@ function getCreateCardDate($cardID, $arrayJSON) {
  * @param mixed $arrayJSON
  * @return void
  */
-function generateCardVersionHistory($arrayJSON) {
+function generateCardVersionHistory($arrayJSON)
+{
     $releaseHistoryId = "5c8aded82d38c74039cf8009"; // the id of the list "Releases / Patch Notes" on Epic's Trello Roadmap
-    $searchResult = ""; $allEntries = ""; $entryURL = ""; $cardID = "";
+    $searchResult = "";
+    $allEntries = "";
+    $entryURL = "";
+    $cardID = "";
     $elem = 0; // just for counting the element's number in the JSON file for easier debugging
-    foreach($arrayJSON["cards"] as $i) {
-        if(array_key_exists( "idList", $i)) {
+    foreach ($arrayJSON["cards"] as $i) {
+        if (array_key_exists("idList", $i)) {
             $searchResult = $i["idList"];
 
-            if($searchResult == $releaseHistoryId) {
+            if ($searchResult == $releaseHistoryId) {
                 $entryURL = $i["url"]; // URL for each card's link
                 $cardID = $i["id"]; // save card's ID for getCreateCardDate()
                 $entryDate = DateTime::createFromFormat('Y-m-d\TH:i:s.u\Z', getCreateCardDate($cardID, $arrayJSON));
                 $entryDateShort = $entryDate->format('Y-m-d');
                 $entryDateLong = $entryDate->format('Y-m-d H:i:s');
 
-                if(array_key_exists("desc", $i)) {
+                if (array_key_exists("desc", $i)) {
                     $entryProgram = $i["name"];
-                    if(ctype_space($i["desc"]) || strlen($i["desc"]) < 1) {
+                    if (ctype_space($i["desc"]) || strlen($i["desc"]) < 1) {
                         $entryLog = "(No Patch Release description was given for this entry)";
                     } else {
                         $searchFor = array("**Patch Notes:**", "**Patch Notes**", "Patch Notes:", "Patch Notes");
@@ -64,12 +69,12 @@ function generateCardVersionHistory($arrayJSON) {
                 // single entry in the html
                 $entry = [
                     "<div>",
-                        "<div class='horizontal-line'></div>",
-                        // "<div class='entryNo'>" . $elem . "</div>",
-                        "<p><a href='" . $entryURL . "' target='_blank' title='Click to open the link to the Trello card in a separate window'>"
+                    "<div class='horizontal-line'></div>",
+                    // "<div class='entryNo'>" . $elem . "</div>",
+                    "<p><a href='" . $entryURL . "' target='_blank' title='Click to open the link to the Trello card in a separate window'>"
                         . $entryProgram . "</a><span class='date' title='"
                         . $entryDateLong . " UTC±00:00 (creation of the Trello card)'> ( " . $entryDateShort . " )</span></p>",
-                        "<p class='changelog'>" . $entryLog . "</div>",
+                    "<p class='changelog'>" . $entryLog . "</div>",
                     "</div>"
                 ];
                 $entry = implode("\n", $entry);
