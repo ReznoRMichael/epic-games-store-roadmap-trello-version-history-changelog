1b6b88dc6f8bf777d9415ee4f141d59042cc2eaa
ReznoRMichael <mic3arena@gmail.com>
2019-09-05 16:02:40 +0200

    Updated Readme and HTML titles, descriptions, icon

--- README.md
+++ README.md
@@ -6,6 +6,18 @@
 
 Written for practice. A simple Trello JSON file parser in PHP for showing the Epic Games Store Release Notes / Changelog on a single page.
 
+## Version History
+
+* 2019-09-02:
+  * First release (used "actions" instead of "cards")
+* 2019-09-03:
+  * Added time of entry
+* 2019-09-04:
+  * Improved readability, no duplicates. Reading "cards" instead of "actions". Added links to the Trello cards.
+* 2019-09-05:
+  * Deleted unneeded "Patch Notes" texts, whitespaces, time of day. Date moved to the same line as Version name. Fixed dates for cards, now the date of card creation on Trello is showed. Refactored code, added comments, formatted files. Mouse hover over date shows also detailed time of day and Timezone (UTC +0).
+  * Added favicon, description, source link, changed titles.
+
 ## Technologies
 
 * HTML5/CSS
--- index.php
+++ index.php
@@ -5,10 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>Epic Games Store Roadmap Trello PHP Parser (Version History / Changelog)</title>
+    <title>Epic Games Store Version History on a single page</title>
+    <meta name="description" content="Epic Games Store Launcher/Web Roadmap Trello JSON PHP parser by ReznoR.">
+	<meta name="keywords" content="epic, games, store, web, client, launcher, version, history, changelog, single, page">
+	
+	<meta property="og:title" content="Epic Games Store Version History on a single page">
+	<meta property="og:description" content="Epic Games Store Launcher/Web Roadmap Trello JSON PHP parser by ReznoR.">
+	<meta property="og:image" content="https://reznor.tech/favicon.png">
+	<meta property="og:url" content="https://reznor.tech/epic-games-store-version-history">
+	<meta name="twitter:card" content="summary_large_image">
+	<meta property="og:site_name" content="rezno[R].tech">
+	<meta name="twitter:image:alt" content="Epic Games Store Launcher/Web Roadmap Trello JSON PHP parser by ReznoR.">
+
+	<meta name="author" content="ReznoR Michael">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<link rel="icon" href="../favicon.png">
 </head>
 <body>
-    <h1>Epic Games Store Roadmap — Version History / Changelog</h1>
+    <h1>Epic Games Store — Version History</h1>
+    <p class="smalltext">Changelog for the Epic Games Store Web & Client (Launcher) collected on a Single Page.<br>
+    Source: official <a href="https://trello.com/b/GXLc34hk/epic-games-store-roadmap" target="_blank">Epic Games Trello Roadmap</a>.</p>
     <p class="smalltext">Trello .json file parser written by ReznoR for fun and learning ( <a href="https://github.com/ReznoRMichael" target="_blank">github.com/ReznoRMichael</a> )<br>
     You can support me directly through <a href="https://www.paypal.me/ReznoRMichael" target="_blank">PayPal</a> if you think it's worth it.</p>
     <div style="margin-bottom: 20px;"></div>
