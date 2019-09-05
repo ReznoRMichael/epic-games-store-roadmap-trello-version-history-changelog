96bd4e40129d81ecf583c0534a787067f9dfc7a6
ReznoRMichael <mic3arena@gmail.com>
2019-09-05 14:16:55 +0200

    getCreateCardDate() - valid creation date

--- generateVersionHistory.php
+++ generateVersionHistory.php
@@ -1,18 +1,25 @@
 <?php
 // Get the contents of the JSON file 
 $strJsonFileContents = file_get_contents("GXLc34hk.json");
-// Convert to array for use with PHP
+// Convert to associative array for use with PHP
 $arrayJSON = json_decode($strJsonFileContents, true);
 // var_dump($arrayJSON); // print array
 
 echo ""; // clear the contents each time
 
-function getCreateCardDate($cardID) {
-;
+function getCreateCardDate($cardID, $arrayJSON) {
+    foreach($arrayJSON["actions"] as $i) {
+        if(array_key_exists("card", $i["data"])) {
+            if($i["data"]["card"]["id"] === $cardID && $i["type"] === "createCard") {
+                return $i["date"];
+            }
+        }
+    }
+    return "2019-01-01T00:00:00.001Z"; // dummy date in case there will be some error
 }
 
 function generateCardVersionHistory($arrayJSON) {
-    $releaseHistoryId = "5c8aded82d38c74039cf8009"; // the id of Version History on Trello
+    $releaseHistoryId = "5c8aded82d38c74039cf8009"; // the id of the list "Releases / Patch Notes" on Epic's Trello Roadmap
     $searchResult = ""; $allEntries = ""; $cardID = "";
     $elem = 0;
     foreach($arrayJSON["cards"] as $i) {
@@ -20,9 +27,9 @@ function generateCardVersionHistory($arrayJSON) {
             $searchResult = $i["idList"];
 
             if($searchResult == $releaseHistoryId) {
-                $entryDate = DateTime::createFromFormat('Y-m-d\TH:i:s.u\Z', $i["dateLastActivity"])->format('Y-m-d');
                 $entryURL = $i["url"];
                 $cardID = $i["id"]; // save card's ID for getCreateCardDate()
+                $entryDate = DateTime::createFromFormat('Y-m-d\TH:i:s.u\Z', getCreateCardDate($cardID, $arrayJSON))->format('Y-m-d');
 
                 if(array_key_exists("desc", $i)) {
                     $entryProgram = $i["name"];
@@ -44,7 +51,6 @@ function generateCardVersionHistory($arrayJSON) {
                     "<div>",
                         "<div class='horizontal-line'></div>",
                         // "<div class='entryNo'>" . $elem . "</div>",
-                        // "<div class='date'>" . $entryDate . "</div>",
                         "<p><a href='" . $entryURL . "' target='_blank' title='Click to open the link to the Trello card in a separate window'>"
                          . $entryProgram . "</a><span class='date'> ( " . $entryDate . " )</span></p>",
                         "<p class='changelog'>" . $entryLog . "</div>",
@@ -58,48 +64,6 @@ function generateCardVersionHistory($arrayJSON) {
         $elem++;
     }
     echo $allEntries;
-}
-
-function generateActionVersionHistory($arrayJSON) {
-    $releaseHistoryId = "5c8aded82d38c74039cf8009"; // the id of Version History on Trello
-    $searchResult = ""; $allEntries = "";
-    echo ""; // clear the contents each time the function is called
-
-    $elem = 0;
-    foreach($arrayJSON["actions"] as $i) {
-        if(array_key_exists( "list", $i["data"])) {
-            $searchResult = $i["data"]["list"]["id"];
-
-            if($searchResult == $releaseHistoryId) {
-                // $entryDate = DateTime::createFromFormat('Y-m-dTH:i:s.u', str_replace("Z", "", $i["date"]))->format('Y-m-d');
-                // var_dump($entryDate);
-                $entryDate = substr($i["date"], 0, 10) . " — " . substr($i["date"], 11, 8);
-                if(array_key_exists("card", $i["data"])) {
-                    if(array_key_exists("desc", $i["data"]["card"])) {
-                        $entryProgram = $i["data"]["card"]["name"];
-                        $entryLog = str_replace("\n", "<br>", $i["data"]["card"]["desc"]);
-                    } else {
-                        $entryProgram = $i["data"]["card"]["name"];
-                        $entryLog = "(No Patch Release description was given for this entry)";
-                    }
-                }
-
-                $entry = [
-                    "<div>",
-                        "<div style='border-bottom:1px solid lightgray;'></div>",
-                        //"<div class='entryNo'>" . $elem . "</div>",
-                        "<div class='date'>" . $entryDate . "</div>",
-                        "<p><strong>" . $entryProgram . "</strong></p>",
-                        "<p class='changelog'>" . $entryLog . "</div>",
-                    "</div>"
-                ];
-                $entry = implode("\n", $entry);
-            
-                $allEntries .= $entry;
-            }
-        }
-        $elem++;
-    }
 }
 
 generateCardVersionHistory($arrayJSON);
