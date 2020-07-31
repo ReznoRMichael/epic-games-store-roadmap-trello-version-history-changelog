1557c1a2f6e9426b3cff628ca770b209249efd86
ReznoRMichael <mic3arena@gmail.com>
2020-08-01 01:46:21 +0200

    switch to REST API, full auto update

--- generateVersionHistory.php
+++ generateVersionHistory.php
@@ -1,12 +1,33 @@
 <?php
 // $execution_time = microtime(true); // Start counting
-// Get the contents of the JSON file 
-$strJsonFileContents = file_get_contents("GXLc34hk.json");
-// Convert to associative array for use with PHP
+$publicKey = ""; // your Trello Public API key
+$trelloBoardId = "5c8aab433718ca7a53ceb3b8"; // the id of the trello board for the API call
+$releaseHistoryId = "5c8aded82d38c74039cf8009"; // the id of the list "Releases / Patch Notes" on Epic's Trello Roadmap
+$URIparameters = "&cards=all&lists=all&actions=all"; // parameters for the REST API call
+$curl = "https://api.trello.com/1/boards/" . $trelloBoardId . "/?key=" . $publicKey . $URIparameters; // REST API call to Trello servers
+//$curl = "GXLc34hk.json"; // can be also a local downloaded .json file directly from Trello instead
+// Put the contents of the JSON API response in a string
+$strJsonFileContents = file_get_contents($curl);
+// Convert the string to associative array for use with PHP
 $arrayJSON = json_decode($strJsonFileContents, true);
-// var_dump($arrayJSON); // print array
+//var_dump($arrayJSON); // view array in console
+// console_log($arrayJSON); // view array in console
 
 echo ""; // clear the contents each time
+
+/**
+ * Logs the JSON to the console for easy debugging using Javascript
+ * @param array $output
+ * @param bool $withScriptTags
+ * @return string
+ */
+function console_log($output, $withScriptTags = true) {
+    $jsCode = "console.log(" . json_encode($output, JSON_HEX_TAG) . ");";
+    if ($withScriptTags) {
+        $jsCode = "<script>" . $jsCode . "</script>";
+    }
+    echo $jsCode;
+}
 
 /**
  * Gets the "createCard" date from the Trello JSON file
@@ -18,7 +39,12 @@ function getCreateCardDate($cardID, $arrayJSON)
 {
     foreach ($arrayJSON["actions"] as $i) {
         if (array_key_exists("card", $i["data"])) {
-            if ($i["data"]["card"]["id"] === $cardID && $i["type"] === "createCard") {
+            if ($i["data"]["card"]["id"] === $cardID) {
+                /* if ($i["type"] === "createCard") {
+                    return $i["date"];
+                } else if ($i["type"] === "updateCard") {
+                    return $i["date"];
+                } */
                 return $i["date"];
             }
         }
@@ -29,11 +55,11 @@ function getCreateCardDate($cardID, $arrayJSON)
 /**
  * Generates the card version history on the page by reading the JSON file
  * @param mixed $arrayJSON
+ * @param string $releaseHistoryId
  * @return void
  */
-function generateCardVersionHistory($arrayJSON)
+function generateCardVersionHistory($arrayJSON, $releaseHistoryId)
 {
-    $releaseHistoryId = "5c8aded82d38c74039cf8009"; // the id of the list "Releases / Patch Notes" on Epic's Trello Roadmap
     $searchResult = "";
     $allEntries = "";
     $entryURL = "";
@@ -46,7 +72,12 @@ function generateCardVersionHistory($arrayJSON)
             if ($searchResult == $releaseHistoryId) {
                 $entryURL = $i["url"]; // URL for each card's link
                 $cardID = $i["id"]; // save card's ID for getCreateCardDate()
-                $entryDate = DateTime::createFromFormat('Y-m-d\TH:i:s.u\Z', getCreateCardDate($cardID, $arrayJSON));
+                $cardDate = getCreateCardDate($cardID, $arrayJSON);
+                    // In case missing/invalid date on the action, switch to the "Last Activity" date on the card instead
+                    if ($cardDate === "2019-01-01T00:00:00.001Z") {
+                        if ($i["dateLastActivity"]) $cardDate = $i["dateLastActivity"];
+                    }
+                $entryDate = DateTime::createFromFormat('Y-m-d\TH:i:s.u\Z', $cardDate);
                 $entryDateShort = $entryDate->format('Y-m-d');
                 $entryDateLong = $entryDate->format('Y-m-d H:i:s');
 
@@ -70,11 +101,11 @@ function generateCardVersionHistory($arrayJSON)
                 $entry = [
                     "<div>",
                     "<div class='horizontal-line'></div>",
-                    // "<div class='entryNo'>" . $elem . "</div>",
+                    // "<div class='entryNo'>" . $elem . " cardId=" . $i["id"] . "</div>",
                     "<p><a href='" . $entryURL . "' target='_blank' rel='noreferrer' title='Click to open the link to the Trello card in a separate window'>"
                         . $entryProgram . "</a><span class='date' title='"
-                        . $entryDateLong . " UTC±00:00 (creation of the Trello card)'> ( " . $entryDateShort . " )</span></p>",
-                    "<p class='changelog'>" . $entryLog . "</div>",
+                        . $entryDateLong . " UTC±00:00 (dates are not accurate!)'> ( " . $entryDateShort . " )</span></p>",
+                    "<p class='changelog'>" . $entryLog . "</p>",
                     "</div>"
                 ];
                 $entry = implode("\n", $entry);
@@ -89,7 +120,7 @@ function generateCardVersionHistory($arrayJSON)
     echo $allEntries;
 }
 
-generateCardVersionHistory($arrayJSON);
+generateCardVersionHistory($arrayJSON, $releaseHistoryId);
 
 // $execution_time = microtime(true) - $execution_time;
 // printf('It took %.5f sec', $execution_time);
--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 	
 	<meta property="og:title" content="Epic Games Store Version History on a single page">
 	<meta property="og:description" content="Epic Games Store Launcher/Web Roadmap Trello JSON PHP parser by ReznoR.">
-	<meta property="og:image" content="https://reznor.tech/favicon.png">
-	<meta property="og:url" content="https://reznor.tech/epic-games-store-version-history">
+	<meta property="og:image" content="http://reznortech.rf.gd/favicon.png">
+	<meta property="og:url" content="http://reznortech.rf.gd/epic-games-store-version-history">
 	<meta name="twitter:card" content="summary_large_image">
 	<meta property="og:site_name" content="rezno[R].tech">
 	<meta name="twitter:image:alt" content="Epic Games Store Launcher/Web Roadmap Trello JSON PHP parser by ReznoR.">
@@ -23,9 +23,9 @@
 </head>
 <body>
     <h1>Epic Games Store — Version History</h1>
-    <p class="smalltext">Changelog for the Epic Games Store Web & Client (Launcher) collected on a Single Page.<br>
+    <p class="smalltext">Changelog for the Epic Games Store Web & Client (Launcher) collected on a Single Page. Updates automatically.<br>
     Source: official <a href="https://trello.com/b/GXLc34hk/epic-games-store-roadmap" target="_blank" rel="noreferrer">Epic Games Trello Roadmap</a>.</p>
-    <p class="smalltext">Trello .json file parser written by ReznoR for fun and learning ( <a href="https://github.com/ReznoRMichael" target="_blank" rel="noreferrer">github.com/ReznoRMichael</a> )<br>
+    <p class="smalltext">Trello REST API .json file parser written by ReznoR for fun and learning ( <a href="https://github.com/ReznoRMichael" target="_blank" rel="noreferrer">github.com/ReznoRMichael</a> )<br>
     You can support me directly through <a href="https://www.paypal.me/ReznoRMichael" target="_blank" rel="noreferrer">PayPal</a> if you think it's worth it.</p>
     <div style="margin-bottom: 20px;"></div>
     <div id="generate">
